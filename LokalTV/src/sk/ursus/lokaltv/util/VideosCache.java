6073144cbfc4a267c06cd6f7ddc2d9938f02925b
Ferko <Ferko@dasdsa>
2014-05-06 02:18:01 +0200

    efdsfds

--- LokalTV/src/sk/ursus/lokaltv/util/VideosCache.java
+++ LokalTV/src/sk/ursus/lokaltv/util/VideosCache.java
@@ -6,23 +6,23 @@ import java.util.Map;
 
 import sk.ursus.lokaltv.model.Video;
 
-import com.awaboom.ursus.agave.LOG;
-
 public class VideosCache {
-	
+
 	private static final Map<String, ArrayList<Video>> sCache = new HashMap<String, ArrayList<Video>>();
-	
+
 	public static ArrayList<Video> get(String cathegoryUrl) {
-		if(!sCache.containsKey(cathegoryUrl)) {
-			return null;
+		synchronized (sCache) {
+			if (!sCache.containsKey(cathegoryUrl)) {
+				return null;
+			}
+			return sCache.get(cathegoryUrl);
 		}
-		return sCache.get(cathegoryUrl);
 	}
-	
+
 	public static void put(String cathegoryUrl, ArrayList<Video> videos) {
 		sCache.put(cathegoryUrl, videos);
 	}
-	
+
 	public static void clear() {
 		sCache.clear();
 	}
